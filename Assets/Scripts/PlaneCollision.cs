57a5176ac0fcec66419bcd088e105e062c060a4a
Mark Frankle <mfrankle@netid>
2018-08-09 09:57:34 -0700

    First Commit

---
+++ Assets/Scripts/PlaneCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneCollision : MonoBehaviour {
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("Trigger!");
+        //Debug.Log("Tag: " + col.gameObject.tag);
+        if (col.gameObject.tag == "DestroyPlane")
+        {
+            Destroy(this.gameObject);
+        }
+        else if (col.gameObject.tag == "ValidPlane")
+        {
+            GetComponent<Note>().isValid = true;
+        }
+}
+}
