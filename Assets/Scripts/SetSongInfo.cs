e667f33618e4df24b1f6ef66fa1de2eb3e6e33a4
Mark Frankle <mfrankle@uw.edu>
2018-08-14 08:11:58 -0700

    Adding some stuff from home

---
+++ Assets/Scripts/SetSongInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetSongInfo : MonoBehaviour {
+    public Text songTextInfo;
+
+    public void SetTextField(string songName, string songAuthor, string songLength)
+    {
+        songTextInfo.text = songName + " by " + songAuthor + ": " + songLength;
+    }
+}
