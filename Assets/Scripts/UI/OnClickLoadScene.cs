b4bc92d938856f3baadf9a26a654fd4baaf397b9
Mark Frankle <mfrankle@uw.edu>
2018-08-30 16:46:37 -0700

    Song menu underway

---
+++ Assets/Scripts/UI/OnClickLoadScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnClickLoadScene : MonoBehaviour {
+
+    public void LoadBySceneIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void LoadBySceneName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
